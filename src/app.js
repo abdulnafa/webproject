1d836b4c7bda295c05803938a472b6abc96f1bad
abdulnafa <88021237+abdulnafa@users.noreply.github.com>
2022-01-18 09:36:39 +0500

    First Node project

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const app=express();
+const path=require('path');
+var hbs = require('hbs');
+const port= 8000;
+const staticPath=path.join(__dirname,"../public");
+const views_path=path.join(__dirname,"../templates/views");
+const path_partial=path.join(__dirname,"../templates/partials");
+
+app.set('view engine', 'hbs');
+app.set('views',views_path)
+hbs.registerPartials(path_partial);
+app.use(express.static(staticPath));
+
+app.get('/',(req,res)=>{
+    res.render("index");
+});
+
+app.get('/about',(req,res)=>{
+    res.render("about");
+});
+
+app.get('/weather',(req,res)=>{
+    res.render("weather");
+});
+
+
+app.get('*',(req,res)=>{
+    res.render("404page");
+});
+app.listen(port,()=>{
+    console.log(`Your App is Listen to Port ${port}`);
+})
